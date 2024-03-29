9ab15bc23b182cdb7c18cc661b023c7adca13b66
komail2004 <68105072+komail2004@users.noreply.github.com>
2024-03-29 12:28:05 -0400

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -9,14 +9,28 @@ def loadText():
 	# Get count of lines in file
 	# Pick one at random and return its contents
 	pass
+
+#Coordinates
+x=800
+y=600
+# hangma = 
 # Classes
 class player:
-	def init()
+	def __init__(self, image, x,y):
+		# self.image = hangman
+		self.x = x
+		self.y = y
 
 
 # Create graphical window for game.
-def hangManWindow():
-	pass
+def hangMan():
+	#Define window.
+	# Add the player (class).
+	# Add the promp.
+	# Display guess correction - Words guessed turn green.
+	# Update the player's x and y with correct or incorrect guesses.
+	#If statements, if player answers wins and otherwiese the hangman dies.
+
 
 def main():
 	pass
