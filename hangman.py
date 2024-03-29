b7a2f642350f62f007dc4920cc6d98ac619c471e
Joshua <160927816+joshuaDeal@users.noreply.github.com>
2024-03-29 12:18:08 -0400

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,7 +1,21 @@
 import pygame
+import math
+import random
+import time
 
-# Read the contents of the text file.
+# Read the contents of the text file. Returns a string.
 def loadText():
+	# Open file
+	# Get count of lines in file
+	# Pick one at random and return its contents
+	pass
+# Classes
+class player:
+	def init()
+
+
+# Create graphical window for game.
+def hangManWindow():
 	pass
 
 def main():
