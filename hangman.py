8195b4e4b9a89d04474c5e0f40f42c6b2bb41c99
Kami <mkbar2020@gmail.com>
2024-04-14 20:02:38 -0400

    Text box

--- hangman.py
+++ hangman.py
@@ -1,92 +1,75 @@
-#!/usr/bin/env python3
-
 import random
 import pygame
-import time
 
-#Initiating Pygame and assigning the x and y
+# Initiating Pygame and assigning the x and y
 pygame.init()
-x= 600	
-y= 800
+x = 600
+y = 800
 
 def window():
-	#Creating the pygame's window
-	#Caption of the game
-	#Editing the window and updates
-	screen = pygame.display.set_mode((x,y))
-	pygame.display.set_caption("Hangperson")
-	screen.fill(("white"))
+    # Creating the pygame's window
+    # Caption of the game
+    # Editing the window and updates
+    screen = pygame.display.set_mode((x, y))
+    pygame.display.set_caption("Hangperson")
+    base_font = pygame.font.Font(None, 32)
+    user_text = ''
+    input_rect = pygame.Rect(200, 200, 140, 32)
+    color_active = pygame.Color('lightskyblue3')
+    color_passive = pygame.Color('chartreuse4')
+    color = color_passive
+    active = False
 
-	#Creating a class for our Hangpersong Characater
-	class hangperson():
-		def __init__(self, image_path, size, position):
-			self.image = pygame.image.load(image_path).convert_alpha()
-			self.image = pygame.transform.scale(self.image, size)  # Assign to self.image, not self.size
-			self.position = position
+    # Creating a class for our Hangperson Character
+    class hangperson():
+        def __init__(self, image_path, size, position):
+            self.image = pygame.image.load(image_path).convert_alpha()
+            self.image = pygame.transform.scale(self.image, size)  # Assign to self.image, not self.size
+            self.position = position
 
-	#Assigning the class to character and creating it
-	character = hangperson("images/person.png", (200,200),(200, 200))
-	#The main loop of the game
-	running = True
-	while running:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				running = False
-		screen.blit(character.image,character.position)
-		pygame.display.flip()
-# Function to get random line from text file.
-# def getText(fileName):
-# 	with open(fileName, 'r') as file:
-# 		lines = file.readlines()
-# 		randomLine = random.choice(lines)
+    # Assigning the class to character and creating it
+    character = hangperson("images/sprite01.png", (200, 200), (200, 200))
 
-# 	return randomLine.replace('\n', '')
+    # The main loop of the game
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if input_rect.collidepoint(event.pos):
+                    active = True
+                else:
+                    active = False
+            if event.type == pygame.KEYDOWN:
+                if active:
+                    if event.key == pygame.K_BACKSPACE:
+                        user_text = user_text[:-1]
+                    else:
+                        user_text += event.unicode
 
-# # Print the prompt. Fills in guesses provided by rights list.
-# def printPrompt(word, rights):
-# 	for i in word:
-# 		if i in rights:
-# 			print(i, end=' ')
-# 		else:
-# 			print("_", end=' ')
+        # it will set background color of screen
+        screen.fill((255, 255, 255))
 
-# 	print()
+        if active:
+            color = color_active
+        else:
+            color = color_passive
+
+        # draw rectangle and argument passed which should
+        # be on screen
+        pygame.draw.rect(screen, color, input_rect)
+
+        text_surface = base_font.render(user_text, True, (0, 0, 0))
+        screen.blit(text_surface, input_rect.topleft)
+        screen.blit(character.image, character.position)
+        pygame.display.flip()
+
+        # Print input text in the terminal
+        print("Input text:", user_text)
 
 def main():
-	window()
-# 	fileName = "wordlist.txt"
-# 	word = getText(fileName).lower()
-# 	max_guesses = 6
-# 	rights = []
-	
-# 	print("Welcome to Hangman!")
-	
-# 	while max_guesses > 0:
-# 		printPrompt(word, rights)
-		
-# 		guess = input("Enter a letter: ").lower()
-		
-# 		if len(guess) != 1 or not guess.isalpha():
-# 			print("Please enter a valid single letter.")
-# 			continue
-		
-# 		if guess in rights:
-# 			print("You already guessed that letter.")
-# 			continue
-		
-# 		rights.append(guess)
-		
-# 		if guess not in word:
-# 			max_guesses -= 1
-# 			print(f"Wrong! You have {max_guesses} guesses left.")
-		
-# 		if all(letter in rights for letter in word):
-# 			printPrompt(word, rights)
-# 			print("Congratulations! You won!")
-# 			break
-		
-# 	if max_guesses == 0:
-# 		print(f"Sorry, you lost. The word was: {word}")
+    window()
 
 if __name__ == "__main__":
-	main()
+    main()
