06c64360d868994a842615ac0600c9e27edb2fc7
Kami <mkbar2020@gmail.com>
2024-04-16 17:34:52 -0400

    new update

--- new-hangman/hangman.py
+++ new-hangman/hangman.py
@@ -1,64 +1,76 @@
-#!/usr/bin/env python3
+import pygame
+import random
 
-import pygame																														   
-import random																														   
+class HangmanGame:
+    def __init__(self, filename):
+        self.word = self.get_word(filename)
+        self.guessed_letters = []
+        self.hangman_status = 0
+        self.win = False
 
-# Function to get random line from text file.																						   
-def get_word(filename):																												 
-	with open(filename, 'r') as file:																								   
-		lines = file.readlines()																										
-		return random.choice(lines).strip().lower()																					 
+    def get_word(self, filename):
+        with open(filename, 'r') as file:
+            lines = file.readlines()
+            return random.choice(lines).strip().lower()
 
-# Pygame initializations																												
-pygame.init()																														   
-screen = pygame.display.set_mode((800, 600))																							
-font = pygame.font.Font(None, 36)																									   
+    def guess(self, letter):
+        if letter not in self.guessed_letters:
+            self.guessed_letters.append(letter)
+            if letter not in self.word:
+                self.hangman_status += 1
+            elif set(self.word) <= set(self.guessed_letters):
+                self.win = True
 
-# Load hangman images																												   
-hangman_images = [pygame.image.load('hangman' + str(i) + '.png') for i in range(0, 7)]												  
+    def is_game_over(self):
+        return self.hangman_status == 6 or self.win
+
+    def get_display_word(self):
+        return ' '.join([letter if letter in self.guessed_letters else '_' for letter in self.word])
+
+class HangmanDisplay:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((800, 600))
+        self.font = pygame.font.Font(None, 36)
+        self.hangman_images = [pygame.image.load(f'hangman{i}.png') for i in range(7)]
+
+    def update(self, hangman_status, display_word):
+        self.screen.fill((255, 255, 255))
+        self.screen.blit(self.hangman_images[hangman_status], (200, 100))
+        word_label = self.font.render(display_word, 1, (0, 0, 0))
+        self.screen.blit(word_label, (50, 500))
+        pygame.display.update()
+
+    def quit(self):
+        pygame.quit()
 
 def main():
-	filename = "wordlist.txt"
-	word = get_word(filename)
-	guessed_letters = []
+    filename = "wordlist.txt"
+    game = HangmanGame(filename)
+    display = HangmanDisplay()
 
-	hangman_status = 0
-	running = True
-	win = False
+    running = True
 
-	while running:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				running = False
-			elif event.type == pygame.KEYDOWN:
-				letter = pygame.key.name(event.key)
-				if len(letter) == 1 and letter.isalpha():
-					if letter not in guessed_letters:
-						guessed_letters.append(letter)
-						if letter not in word:
-							hangman_status += 1
-						elif set(word) <= set(guessed_letters):
-							win = True
-							running = False
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                letter = pygame.key.name(event.key)
+                if len(letter) == 1 and letter.isalpha():
+                    game.guess(letter)
 
-		screen.fill((255, 255, 255))
-		screen.blit(hangman_images[hangman_status], (200, 100))
+        display.update(game.hangman_status, game.get_display_word())
 
-		word_label = font.render(' '.join([letter if letter in guessed_letters 
-										   else '_' for letter in word]), 1, (0, 0, 0))
-		screen.blit(word_label, (50, 500))
+        if game.is_game_over():
+            running = False
 
-		pygame.display.update()
-		if hangman_status == 6:
-			running = False
+    display.quit()
 
-	if win:
-		print("Congratulations! You won!")
-	else:
-		print(f"Sorry, you lost. The word was: {word}")
-
-	pygame.quit()
+    if game.win:
+        print("Congratulations! You won!")
+    else:
+        print(f"Sorry, you lost. The word was: {game.word}")
 
 if __name__ == "__main__":
-	main()
-
+    main()
